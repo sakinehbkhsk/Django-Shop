9599d89c89ebfbdbb7ca5ec6c1d22d0d5e738506
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-07-19 10:40:51 +0430

    add usermanager model in account app

--- shop/account/models.py
+++ shop/account/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number,password, email=None):
+        if not phone_number:
+            raise ValueError('Enter a phone number.')
+        
+        user = self.model(phone_number=phone_number, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, phone_number, password, email=None):
+        user = self.create_user(phone_number, password, email)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+    
+
+
