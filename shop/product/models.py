b1063a749be8b1139d4db4b7342a24c54e19b23a
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-07-19 14:21:20 +0430

    add offer model in product app

--- shop/product/models.py
+++ shop/product/models.py
@@ -7,12 +7,28 @@ class Category(models.Model):
     def __str__(self) -> str:
         return self.name
     
+class Offer(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    price = models.PositiveBigIntegerField()
+    code = models.CharField(max_length=100, unique=True)
+    discount = models.SmallIntegerField()
+    is_available = models.BooleanField()
+    
+    @property
+    def discount_to_price(self):
+        if self.discount > 0:
+            total_price = self.price - (self.price * self.discount / 100)
+            return float(total_price)
+        return 0
+
+    
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.PositiveBigIntegerField()
     image = models.ImageField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     is_available = models.BooleanField(default=True)
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
