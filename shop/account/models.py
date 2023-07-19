0f28fde0651f0906c47f79754d7de0ff611bdacd
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-07-19 11:21:17 +0430

    add user model in accout app

--- shop/account/models.py
+++ shop/account/models.py
@@ -17,6 +17,32 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+class User(AbstractBaseUser):
+    phone_number = models.CharField(max_length=20, unique=True)
+    email =  models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'phone_number'
     
+    def __str__(self):
+        return self.phone_number
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self,app_lable):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+
+    
+
+
 
 
