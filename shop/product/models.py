d67ecaf6fdf736a4e7ff120f94c21da66707a968
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-07-19 12:40:19 +0430

    add catgory model in product app

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
