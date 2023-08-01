6840b188e30fb26ff8ad4d658ff8de3b7417d2a2
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-01 09:16:04 +0430

    feat: create admin file

--- shop/account/admin.py
+++ shop/account/admin.py
@@ -1,4 +1,35 @@
 from django.contrib import admin
-from .models import User
-# Register your models here.
-admin.site.register(User)
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import Group
+from .models import User, OtpCode
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'full_name', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_admin', 'last_login')}),
+    )
+    add_fieldsets = (
+        (None, {'fields':('phone_number', 'email', 'full_name', 'password1', 'password2')}),
+    )
+    search_fields = ('email', 'full_name')
+    ordering = ('full_name',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'code', 'created')
+
+
