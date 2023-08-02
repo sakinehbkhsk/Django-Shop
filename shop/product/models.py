ecdf1e48b2e3fec03c21e42082b1b7ad6079065c
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 09:39:36 +0430

    feat: add subcategory

--- shop/product/models.py
+++ shop/product/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from core.models import BaseModel
 
 class Category(BaseModel):
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='scategory', null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True)
 
     def __str__(self) -> str:
         return self.name
     
-# selfrelation to category
+
     
 class Product(BaseModel):
     name = models.CharField(max_length=200)
