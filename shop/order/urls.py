ee9fb27214dd9aa44e4231bce29cf6dc2bb7cdeb
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:41:29 +0430

    fix:create offerapply url

--- shop/order/urls.py
+++ shop/order/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
+    path('apply/<int:order_id>/', views.OfferApplyView.as_view(), name='apply_offer')
 
 ]
