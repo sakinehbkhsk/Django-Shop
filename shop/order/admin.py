743f8968434dc25528f3dd7c9e14d3d9d4ce0308
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 13:05:53 +0430

    fix:register orderitem

--- shop/order/admin.py
+++ shop/order/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Offer,Order, ProductOrder
+from .models import Offer,Order, OrderItem
 
 admin.site.register(Order)
 admin.site.register(Offer)
-admin.site.register(ProductOrder)
+admin.site.register(OrderItem)
 
