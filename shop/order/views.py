3de63cd379bfa22f967b5d5809ec63e2ba0331af
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:36:06 +0430

    fix:create cartadd view

--- shop/order/views.py
+++ shop/order/views.py
@@ -4,12 +4,23 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order, OrderItem
 from .cart import Cart
 from .forms import CartAddForm, OfferApplyForm
+from product.models import Product
 
 
 class CartView(View):
     def get(self,request):
         cart = Cart(request)
         return render(request, 'order/cart.html', {'cart': cart})
+     
+
+class CartAddView(View):
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('order:cart')
     
 
 class OrderDetailView(LoginRequiredMixin, View):
