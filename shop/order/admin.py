01a99fcd0e733a959db5098ca7115c01943214d3
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:20:53 +0430

    fix:register order models

--- shop/order/admin.py
+++ shop/order/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Offer,Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(Offer)
-admin.site.register(OrderItem)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user','paid')
+    list_filter = ('paid',)
+    inlines = (OrderItemInline,)
 
