fedb4508c716f1c9b9d740c7feeaf58c3eb533c2
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:28:57 +0430

    feat:create order detail view

--- shop/order/views.py
+++ shop/order/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order, OrderItem
 from .cart import Cart
+from .forms import CartAddForm, OfferApplyForm
+
+
+class OrderDetailView(LoginRequiredMixin, View):
+    form_class = OfferApplyForm
+
+    def get(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id)
+        return render(request, 'order/order.html', {'order':order, 'form':self.form_class})
+
 
 class OrderCreateView(LoginRequiredMixin, View):
     def get(self, request):
