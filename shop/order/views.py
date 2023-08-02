038c89bcdbf3fac0f78febb911e6a7283b578eb3
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:41:54 +0430

    feat:creat offer apply view

--- shop/order/views.py
+++ shop/order/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Offer
 from .cart import Cart
 from .forms import CartAddForm, OfferApplyForm
 from product.models import Product
+import datetime
+from django.contrib import messages
 
 
 class CartView(View):
@@ -47,3 +49,22 @@ class OrderCreateView(LoginRequiredMixin, View):
             OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
         cart.clear()
         return redirect('order:order_detail', order.id)
+
+
+class OfferApplyView(LoginRequiredMixin,View):
+    form_class = OfferApplyForm
+
+    def post(self, request, order_id):
+        now = datetime.datetime.now()
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            try:
+                offer = Offer.objects.get(code__exact=code, start_time__lte=now, end_time__gte=now, active=True)
+            except Offer.DoesNotExist:
+                messages.error(request, 'this coupon does not exists.', 'danger')
+                return redirect('order:order_detail', order_id)
+            order = Order.objects.get(id=order_id)
+            order.discount = offer.discount
+            order.save()
+        return redirect('order:order_detail', order_id)
