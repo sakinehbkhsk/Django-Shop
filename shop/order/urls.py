70cdb808f54ed4562e2b415ce10ce8939f75f7d0
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:32:53 +0430

    feat:create cart url

--- shop/order/urls.py
+++ shop/order/urls.py
@@ -5,5 +5,6 @@ app_name = 'order'
 urlpatterns = [
     path('create/', views.OrderCreateView.as_view(), name='order_create'),
     path('detail/<int:order_id>/', views.OrderDetailView.as_view(), name='order_detail'),
+    path('cart/', views.CartView.as_view(), name='cart'),
 
 ]
