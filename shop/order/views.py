64a4589ff24e5e292f930b2b25581a1dd50b54d2
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:22:12 +0430

    fix:create orderview

--- shop/order/views.py
+++ shop/order/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Order, OrderItem
+from .cart import Cart
 
-# Create your views here.
+class OrderCreateView(LoginRequiredMixin, View):
+    def get(self, request):
+        cart = Cart(request)
+        order = Order.objects.create(user=request.user)
+        for item in cart:
+            OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+        cart.clear()
+        return redirect('order:order_detail', order.id)
