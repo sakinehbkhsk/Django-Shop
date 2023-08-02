0e55f91a9e087cbdd65b811bebb9e7b24a9761de
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 10:31:52 +0430

    fix:change orderitem model

--- shop/order/models.py
+++ shop/order/models.py
@@ -39,13 +39,16 @@ class Order(BaseModel):
         return total    
 
 
-class ProductOrder(BaseModel):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='productorders')
+class OrderItem(BaseModel):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.PositiveBigIntegerField()
 
-    def total_price(self):
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
         return self.price * self.quantity
     
 
