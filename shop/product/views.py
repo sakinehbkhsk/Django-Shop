cbb7390ed1bcb7ceb02645791fe6b15ca2a4e2e4
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 10:04:48 +0430

    feat:create productview

--- shop/product/views.py
+++ shop/product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .models import Product, Category
 
-# Create your views here.
+
+
+class ProductView(View):
+    def get(self, request, category_slug=None):
+        products = Product.objects.filter(available=True)
+        categories = Category.objects.filter(is_sub=False)
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'product/product.html', {'products':products, 'categories':categories})
