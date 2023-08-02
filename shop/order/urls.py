c431966ed5df695f31d5b4765bdede4c04510968
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 16:37:18 +0430

    fix:create cartremove url

--- shop/order/urls.py
+++ shop/order/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('detail/<int:order_id>/', views.OrderDetailView.as_view(), name='order_detail'),
     path('cart/', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
+    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
 
 ]
