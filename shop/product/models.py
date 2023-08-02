36553697614214b769b4bf360f90df4462329a3b
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-02 09:40:54 +0430

    fix:add meta class to category class

--- shop/product/models.py
+++ shop/product/models.py
@@ -7,6 +7,12 @@ class Category(BaseModel):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True)
 
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
     def __str__(self) -> str:
         return self.name
     
