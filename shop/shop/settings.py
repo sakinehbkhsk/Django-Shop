33b6a34aced10896dfe6632091dce955b899091b
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-16 23:48:05 +0430

    feat:add sandbox mode

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -139,3 +139,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
+
+
+
+# SANDBOX MODE
+
+MERCHANT = "00000000-0000-0000-0000-000000000000"
+
+SANDBOX = True
+
