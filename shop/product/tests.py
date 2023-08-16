52e2a993a02a9e6058076c9525c8a822608cd7a9
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-16 11:37:09 +0430

    feat: create product tests

--- shop/product/tests.py
+++ shop/product/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Product, Category
+from . import views
 
-# Create your tests here.
+
+class ProductViewTest(TestCase):
+    def test_product_list_view(self):
+        response = self.client.get(reverse('product:product'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product/product.html')
+    
+    def test_category_filter_view(self):
+        category = Category.objects.create(name='Test Product', slug='test')
+        response = self.client.get(reverse('product:category_filter', kwargs={'category_slug': category.slug}))
+        self.assertEqual(response.status_code, 200)
+
+
+# class ProductDetailViewTest(TestCase):
+#     def setUp(self):
+#         self.product = Product.objects.create(name='Test Product', slug='testpro')
+    
+#     def test_product_detail_view(self):
+#         response = self.client.get(reverse('product:product_detail', kwargs={'slug': self.product.slug}))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'product/detail.html')
+
+
+class SearchProductTest(TestCase):
+    def test_search_product_view(self):
+        response = self.client.get(reverse('product:search_product'), {'q': 'test query'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product/search.html')
+
+    
