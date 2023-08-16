fd8087f8d5b253478a79b3d56e77c36bfba9f7b2
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-16 23:49:17 +0430

    feat:create payment order view

--- shop/order/views.py
+++ shop/order/views.py
@@ -7,6 +7,10 @@ from .forms import CartAddForm, OfferApplyForm
 from product.models import Product
 import datetime
 from django.contrib import messages
+from django.conf import settings
+import requests
+import json
+from django.http import JsonResponse
 
 
 class CartView(View):
@@ -69,3 +73,71 @@ class OfferApplyView(LoginRequiredMixin,View):
             order.discount = offer.discount
             order.save()
         return redirect('order:order_detail', order_id)
+
+
+#? sandbox merchant 
+if settings.SANDBOX:
+    sandbox = 'sandbox'
+else:
+    sandbox = 'www'
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  
+CallbackURL = 'http://127.0.0.1:8080/order/verify/'
+
+
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        request.session['order_pay'] = {
+            'order_id': order.id,
+        }
+        data = {
+            "MerchantID": settings.MERCHANT,
+            "Amount": order.get_total_price(),
+            "Description": description,
+            "Phone": request.user.phone_number,
+            "CallbackURL": CallbackURL,
+        }
+        data = json.dumps(data)
+        headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+        try:
+            response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+            if response.status_code == 200:
+                response_data = response.json()
+                if response_data['Status'] == 100:
+                    return JsonResponse({'status': True, 'url': ZP_API_STARTPAY + str(response_data['Authority']), 'authority': response_data['Authority']})
+                else:
+                    return JsonResponse({'status': False, 'code': str(response_data['Status'])})
+            return JsonResponse(response_data)
+        
+        except requests.exceptions.Timeout:
+            return JsonResponse({'status': False, 'code': 'timeout'})
+        except requests.exceptions.ConnectionError:
+            return JsonResponse({'status': False, 'code': 'connection error'})
+
+
+class OrderVerifyView(LoginRequiredMixin, View):
+    def get(self, request, authority):
+        order_id = request.session['order_pay']['order_id']
+        order = Order.objects.get(id=int(order_id))
+        data = {
+        "MerchantID": settings.MERCHANT,
+        "Amount": order.get_total_price(),
+        "Authority": authority,
+        }
+        data = json.dumps(data)
+        # set content length by data
+        headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
+        response = requests.post(ZP_API_VERIFY, data=data,headers=headers)
+
+        if response.status_code == 200:
+            response = response.json()
+            if response['Status'] == 100:
+                return JsonResponse({'status': True, 'RefID': response['RefID']})
+            else:
+                return JsonResponse({'status': False, 'code': str(response['Status'])})
+        return response
