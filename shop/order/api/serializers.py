d0c849430ac40cdbc36fe304361900182a9b52e2
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-08-31 20:00:33 +0430

    fix:create order  serializers

--- shop/order/api/serializers.py
+++ shop/order/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Order, OrderItem, Offer
+from ..models import Order, OrderItem, Offer
 
 
 class OrderSerializer(serializers.ModelSerializer):
