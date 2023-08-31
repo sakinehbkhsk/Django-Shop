f6447a708e63bf1cd0e1294cdb750e2ffe1f74da
sakinehM93 <sakineh75pourbakhshali@gmail.com>
2023-09-01 00:15:51 +0430

    fix:change some minor

--- shop/product/urls.py
+++ shop/product/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# from .views import ProductListView, ProductView
 from . import views
 
 app_name = 'product'
@@ -8,9 +7,5 @@ urlpatterns = [
     path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('category/<slug:category_slug>/', views.ProductView.as_view(), name='category_filter'),
     path('search_product', views.SearchProduct.as_view(), name='search_product'),
-    
 
-    # path('', ProductListView.as_view(), name='api-products'),
-    # path('api/products/<slug:category_slug>/', ProductListView.as_view(), name='api-products-by-category'),
-    
 ]
